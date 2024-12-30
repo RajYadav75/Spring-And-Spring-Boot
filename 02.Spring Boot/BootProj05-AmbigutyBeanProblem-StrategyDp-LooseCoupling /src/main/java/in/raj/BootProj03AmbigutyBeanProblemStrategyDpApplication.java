92140c3288b863daf65b6b65e62c789e76d33c14
RajYadav75 <rajyadav751811@gmail.com>
2024-12-30 09:17:23 +0530

    Solved Ambiguty prblm uzig @Qualifier and @Lazy for not showing other object

---
+++ 02.Spring Boot/BootProj05-AmbigutyBeanProblem-StrategyDp-LooseCoupling /src/main/java/in/raj/BootProj03AmbigutyBeanProblemStrategyDpApplication.java
@@ -0,0 +1,25 @@
+package in.raj;
+
+import in.raj.sbeans.Vehicle;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("in/raj/cfgs/ApplicationContext.xml")
+public class BootProj03AmbigutyBeanProblemStrategyDpApplication {
+
+	public static void main(String[] args) {
+		try(ConfigurableApplicationContext applicationContext =
+					SpringApplication.run(BootProj03AmbigutyBeanProblemStrategyDpApplication.class, args);){
+			Vehicle vehicle = applicationContext.getBean(Vehicle.class);
+			vehicle.move("Gorakhpur","Surat");
+
+
+		}catch (Exception e){
+			System.out.println(e);
+		}
+	}
+
+}
